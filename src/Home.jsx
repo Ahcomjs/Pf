22143118b70205236f6f80c843a338c2b1abefaf
Carlos <carlos.delacruz228@gmail.com>
2024-04-02 13:09:22 -0400

    Under construction page

---
+++ src/Home.jsx
@@ -0,0 +1,17 @@
+import './index.css';
+
+const Home = () => {
+
+
+  return (
+    <> 
+      <div className='container'>
+        <div className="content">
+         
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
